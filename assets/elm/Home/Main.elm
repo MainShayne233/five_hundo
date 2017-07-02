38173368dd7eb000fabaf70a4129bc5febe54bf5
MainShayne233 <shaynetremblay@hotmail.com>
2017-07-02 01:44:15 -0400

    formatted

--- assets/elm/Home/Main.elm
+++ assets/elm/Home/Main.elm
@@ -1,14 +1,17 @@
 module Main exposing (..)
 
-import Html exposing (program, Html)
-import Models exposing (init)
-import Update exposing (update)
-import Html exposing (Html, button, div, h2, input, p, text, textarea)
+import Debounce exposing (Debounce)
+import Html exposing (Html, button, div, h2, input, p, program, text, textarea)
 import Html.Attributes exposing (class, cols, placeholder, rows, style, type_)
 import Html.Events exposing (onClick, onInput)
-import Models exposing (..)
-import Msgs exposing (..)
+import Http exposing (post)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (decode, hardcoded, optional, required, requiredAt)
+import Json.Encode as Encode
 import Regex exposing (regex, split)
+import Task
+import Time exposing (second)
+
 
 -- APP
 
@@ -20,6 +23,344 @@ main =
         , subscriptions = always Sub.none
         , view = view
         }
+
+
+
+-- MODEL
+
+
+type Action
+    = Typing
+    | Saved
+    | Idle
+
+
+type Authorization
+    = Authorized
+    | NotAuthorized
+    | Checking
+
+
+type alias Model =
+    { entry : Entry
+    , breakdown : Breakdown
+    , persistDebounce : Debounce String
+    , setIdleDebounce : Debounce String
+    , action : Action
+    , authorization : Authorization
+    , passwordMessage : String
+    }
+
+
+type alias Breakdown =
+    List Entry
+
+
+type alias Entry =
+    String
+
+
+type alias EntryResponse =
+    { entry : Entry, breakdown : Breakdown }
+
+
+type alias AuthorizationResponse =
+    { authorized : Bool
+    , breakdown : List String
+    }
+
+
+model : Model
+model =
+    { entry = ""
+    , breakdown = []
+    , persistDebounce = Debounce.init
+    , setIdleDebounce = Debounce.init
+    , action = Idle
+    , authorization = Checking
+    , passwordMessage = ""
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model
+    , checkIfAuthorized
+    )
+
+
+persistDebounceConfig : Debounce.Config Msg
+persistDebounceConfig =
+    { strategy = Debounce.later (0.5 * second)
+    , transform = PersistDebounce
+    }
+
+
+setIdleDebounceConfig : Debounce.Config Msg
+setIdleDebounceConfig =
+    { strategy = Debounce.later (0.5 * second)
+    , transform = SetIdleDebounce
+    }
+
+
+
+-- MSGS
+
+
+type Msg
+    = Change String
+    | PasswordChange String
+    | PasswordResponse (Result Http.Error String)
+    | SessionResponse (Result Http.Error AuthorizationResponse)
+    | SubmitPassword String
+    | PersistSuccess (Result Http.Error String)
+    | InitialEntry (Result Http.Error { entry : String, breakdown : List String })
+    | PersistDebounce Debounce.Msg
+    | SetIdleDebounce Debounce.Msg
+    | PersistEntry String
+    | SetIdle String
+
+
+
+-- CMDS
+
+
+persistEntry : String -> Cmd Msg
+persistEntry entry =
+    Task.perform PersistEntry (Task.succeed entry)
+
+
+setIdle : String -> Cmd Msg
+setIdle str =
+    Task.perform SetIdle (Task.succeed str)
+
+
+
+-- ENCODERS
+
+
+encodeEntry : String -> Encode.Value
+encodeEntry entry =
+    Encode.object
+        [ ( "entry", entry |> Encode.string ) ]
+
+
+encodePassword : String -> Encode.Value
+encodePassword password =
+    Encode.object
+        [ ( "password", password |> Encode.string ) ]
+
+
+
+-- DECODERS
+
+
+stringListDecoder : Decoder (List String)
+stringListDecoder =
+    Decode.list Decode.string
+
+
+entryDecoder : Decoder EntryResponse
+entryDecoder =
+    decode EntryResponse
+        |> required "entry" Decode.string
+        |> required "breakdown" stringListDecoder
+
+
+authorizationResponseDecoder : Decoder AuthorizationResponse
+authorizationResponseDecoder =
+    decode AuthorizationResponse
+        |> required "authorized" Decode.bool
+        |> required "breakdown" stringListDecoder
+
+
+
+-- HTTP
+
+
+fetchEntry : Cmd Msg
+fetchEntry =
+    let
+        request =
+            Http.get "/api/entries/today" entryDecoder
+    in
+    Http.send InitialEntry request
+
+
+checkIfAuthorized : Cmd Msg
+checkIfAuthorized =
+    let
+        request =
+            Http.get "/api/authorization/session" authorizationResponseDecoder
+    in
+    Http.send SessionResponse request
+
+
+
+-- REQUESTS
+
+
+submitPassword : String -> Cmd Msg
+submitPassword password =
+    Http.send PasswordResponse (submitPasswordRequest password)
+
+
+submitPasswordRequest : String -> Http.Request String
+submitPasswordRequest password =
+    let
+        encodedPassword =
+            encodePassword password
+    in
+    post
+        "/api/authorization/authorize"
+        (Http.stringBody "application/json" <| Encode.encode 0 <| encodedPassword)
+        Decode.string
+
+
+postEntry : String -> Cmd Msg
+postEntry entry =
+    Http.send PersistSuccess (postEntryRequest entry)
+
+
+postEntryRequest : String -> Http.Request String
+postEntryRequest entry =
+    let
+        encodedEntry =
+            encodeEntry entry
+    in
+    post
+        "/api/entries/save"
+        (Http.stringBody "application/json" <| Encode.encode 0 <| encodeEntry entry)
+        Decode.string
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InitialEntry (Ok { entry, breakdown }) ->
+            ( { model | entry = entry, breakdown = breakdown }
+            , Cmd.none
+            )
+
+        InitialEntry (Err response) ->
+            ( { model | entry = "" }
+            , Cmd.none
+            )
+
+        Change body ->
+            let
+                ( debounce, cmd ) =
+                    Debounce.push persistDebounceConfig body model.persistDebounce
+            in
+            ( { model | entry = body, persistDebounce = debounce, action = Typing }
+            , cmd
+            )
+
+        PasswordChange password ->
+            ( { model | entry = password }
+            , Cmd.none
+            )
+
+        SubmitPassword password ->
+            ( { model | passwordMessage = "checking..." }
+            , submitPassword password
+            )
+
+        PasswordResponse (Ok response) ->
+            case response of
+                "authorized" ->
+                    ( { model | authorization = Authorized }
+                    , fetchEntry
+                    )
+
+                other ->
+                    ( { model | passwordMessage = "try again" }
+                    , Cmd.none
+                    )
+
+        PasswordResponse (Err response) ->
+            ( { model | entry = response |> toString }
+            , Cmd.none
+            )
+
+        SessionResponse (Ok response) ->
+            case response.authorized of
+                True ->
+                    let
+                        _ =
+                            Debug.log "Hello Elm!"
+                    in
+                    ( { model | authorization = Authorized }
+                    , fetchEntry
+                    )
+
+                other ->
+                    let
+                        _ =
+                            Debug.log "Hello Elm!"
+                    in
+                    ( { model | authorization = Authorized }
+                    , Cmd.none
+                    )
+
+        SessionResponse (Err response) ->
+            let
+                _ =
+                    Debug.log "Hello Elm!"
+            in
+            ( { model | authorization = NotAuthorized }
+            , Cmd.none
+            )
+
+        PersistSuccess response ->
+            let
+                ( debounce, cmd ) =
+                    Debounce.push setIdleDebounceConfig "" model.setIdleDebounce
+            in
+            ( { model | setIdleDebounce = debounce }
+            , cmd
+            )
+
+        PersistDebounce msg ->
+            let
+                ( debounce, cmd ) =
+                    Debounce.update
+                        persistDebounceConfig
+                        (Debounce.takeLast persistEntry)
+                        msg
+                        model.persistDebounce
+            in
+            ( { model | persistDebounce = debounce }
+            , cmd
+            )
+
+        SetIdleDebounce msg ->
+            let
+                ( debounce, cmd ) =
+                    Debounce.update
+                        setIdleDebounceConfig
+                        (Debounce.takeLast setIdle)
+                        msg
+                        model.setIdleDebounce
+            in
+            ( { model | setIdleDebounce = debounce }
+            , cmd
+            )
+
+        PersistEntry entry ->
+            ( { model | action = Saved }
+            , postEntry entry
+            )
+
+        SetIdle str ->
+            ( { model | action = Idle }
+            , Cmd.none
+            )
+
 
 
 -- VIEW
@@ -110,7 +451,7 @@ wordCount body =
         words =
             split Regex.All (regex " |\n") body
     in
-        List.filter isWord words
+    List.filter isWord words
         |> List.length
 
 
